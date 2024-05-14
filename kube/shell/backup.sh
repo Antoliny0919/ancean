3b9e5ed06aa5332ba9b14989f468c3e76f307cc7
Antoliny0919 <antoliny0919@gmail.com>
2024-05-14 14:31:51 +0900

    prod docker-compose volume update, postgres backup folder

--- kube/shell/backup.sh
+++ kube/shell/backup.sh
@@ -1,2 +1,2 @@
 kubectl exec deploy/ancean-postgres-deploy -n $1 -- \
-pg_dump -d postgres -U ancean -F t > /backup/ancean_data_$(date +\%Y\%m\%d).tar
+pg_dump -d postgres -U ancean -F t > /backup/ancean_data_$(date +\%Y-\%m-\%d_%T).tar
--- swarm/shell/install.sh
+++ swarm/shell/install.sh
@@ -14,7 +14,23 @@ sudo apt-get update
 
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+sudo usermod -aG docker $USER
+
+# install docker-compose
+
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
+
+# install aws-cli
+
+sudo apt-get install unzip
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
+
+unzip awscliv2.zip
+
+sudo ./aws/install
+
+rm -rf $(ls)
 
