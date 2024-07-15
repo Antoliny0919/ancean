e5c4cce6c2ee07c3d593b952bc923c413990a8aa
antoliny0919 <antoliny0919@gmail.com>
2024-07-16 07:29:07 +0900

    jenkins db-backup pipeline clear step remove

--- jenkins/pipeline/db-backup/deploy.sh
+++ jenkins/pipeline/db-backup/deploy.sh
@@ -7,8 +7,12 @@ echo "**************************************************"
 AWS_ACCESS_KEY_ID=$1
 AWS_SECRET_ACCESS_KEY=$2
 TARGET_FILE=$3
-TRANSFER_DATA_PATH="$HOME/ancean/backup"
+TRANSFER_DATA_PATH="/var/jenkins_home"
 BUCKET_PATH="ancean-bucket/backup"
+
+echo "Gets the backup file from the database container."
+
+docker cp $(docker ps -q -f name=ancean_db | head -n1):/backup/$TARGET_FILE $TRANSFER_DATA_PATH
 
 echo "Configure AWS IAM"
 
@@ -20,3 +24,10 @@ aws configure set output "text"
 echo "Deploy backup file > s3://$BUCKET_PATH/$TARGET_FILE"
 
 aws s3 cp $TRANSFER_DATA_PATH/$TARGET_FILE s3://$BUCKET_PATH/$TARGET_FILE
+
+echo "Remove backup file in local/container"
+
+docker exec -it $(docker ps -q -f name=ancean_db | head -n1) \
+sh -c "rm -rf /backup/$TARGET_FILE"
+
+rm -rf $TRANSFER_DATA_PATH/$TARGET_FILE
